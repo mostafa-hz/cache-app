ee6c8d42d025654b4efcc1d6768b8563648caeac
Mostafa <forcemos@gmail.com>
2021-09-01 13:22:49 +0300

    Implement Mongodb Connection

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,17 @@ import dotenv from 'dotenv';
 import express, {Router} from 'express';
 import {initCacheModule} from "./modules/cache/init-cache-module";
 import config from "@config";
+import mongoose from 'mongoose';
+
+mongoose.connect(
+    config.mongodbConnection,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    }
+);
 
 dotenv.config();
 const app = express();
