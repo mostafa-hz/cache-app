d47a4b21f080dcc1bc08471376df84c357d5813f
Mostafa <forcemos@gmail.com>
2021-09-01 16:59:38 +0300

    Implement Sample Test2

--- src/modules/cache/services/cache-service.unit.spec.ts
+++ src/modules/cache/services/cache-service.unit.spec.ts
@@ -1,6 +1,7 @@
 import {CacheService} from "@modules/cache/services/cache.service";
 import {CacheRepository} from "@modules/cache/repositories/cache.repository";
 import {Types} from 'mongoose'
+import config from "@config";
 
 describe('Cache Service', () => {
     describe('getValueByKey', () => {
@@ -24,6 +25,31 @@ describe('Cache Service', () => {
 
             const result = await CacheService.getValueByKey(key);
             expect(spy1).toBeCalled();
+            expect(result).toEqual(value)
+        });
+
+        it('should return new value', async () => {
+            const key = 'foo';
+            const value = '6osfeg4';
+            const doc = {
+                key,
+                value,
+                _id: Types.ObjectId(),
+                expireAt: new Date(),
+                updatedAt: new Date(),
+                createdAt: new Date(),
+            };
+            jest.spyOn(Math, 'random').mockReturnValue(0.4674465405198025);
+            const now =  Date.now()
+            jest.spyOn(Date, 'now').mockReturnValue(now);
+            const spy1 = jest.spyOn(CacheRepository, 'findByKey').mockResolvedValueOnce(null);
+            const spy2 = jest.spyOn(CacheRepository, 'upsertValue').mockResolvedValueOnce();
+            const spy3 = jest.spyOn(CacheRepository, 'findByKey').mockResolvedValueOnce(doc);
+
+            const result = await CacheService.getValueByKey(key);
+            expect(spy1).toBeCalled();
+            expect(spy2).toBeCalledWith(key, value, new Date(now + config.cacheTtl));
+            expect(spy3).toBeCalled();
             expect(result).toEqual(value)
         });
     })
