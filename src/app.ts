b6260ba6ec8a96932d7f83f4402dcd620f95e5c2
Mostafa <forcemos@gmail.com>
2021-09-01 11:22:04 +0300

    Init Commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 8090; // default port to listen
+
+// define a route handler for the default home page
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+    // console.log(`server started at http://localhost:${port}`);
+});
