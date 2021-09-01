49c8fc5c4857f15539b154b6a33823cdbbe46895
Mostafa <forcemos@gmail.com>
2021-09-01 13:52:48 +0300

    Implement Cache Model
    with ttl index

---
+++ src/modules/cache/models/cache.model.ts
@@ -0,0 +1,22 @@
+import {Types} from "mongoose";
+import {getModelForClass, modelOptions, prop} from "@typegoose/typegoose";
+
+@modelOptions({schemaOptions: {timestamps: true}})
+export class CacheModel {
+    _id!: Types.ObjectId;
+
+    @prop({required: true, unique: true})
+    key!: string;
+
+    @prop({required: true})
+    value!: string;
+
+    @prop({required: true, type: Date, expires: 0})
+    expireAt!: Date;
+
+    createdAt!: Date;
+
+    updatedAt!: Date;
+}
+
+export const cacheModel = getModelForClass(CacheModel);
