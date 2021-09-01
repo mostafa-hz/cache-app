a1ef3dd2cdde2239e6f7eb0de786a54941f2b0c9
Mostafa <forcemos@gmail.com>
2021-09-01 15:04:28 +0300

    Connect Cache Service To Controllers

--- src/modules/cache/controllers/cache.controller.ts
+++ src/modules/cache/controllers/cache.controller.ts
@@ -1,24 +1,64 @@
 import {NextFunction, Request, Response} from "express";
-import {NotImplementedError} from "@common/errors/NotImplementedError";
+import {CacheService} from "@modules/cache/services/cache.service";
 
 export default {
     async getValue(req: Request, res: Response, next: NextFunction) {
-        next(new NotImplementedError());
+        try {
+            const {params} = req;
+            const {key} = params;
+            const result = await CacheService.getValueByKey(key);
+
+            res.json({
+                value: result
+            });
+        } catch (e) {
+            next(e);
+        }
     },
 
     async getKeys(req: Request, res: Response, next: NextFunction) {
-        next(new NotImplementedError())
+        try {
+            const result = await CacheService.getAllKeys();
+
+            res.json({
+                keys: result
+            });
+        } catch (e) {
+            next(e);
+        }
     },
 
     async putValue(req: Request, res: Response, next: NextFunction) {
-        next(new NotImplementedError())
+        const {params} = req;
+        const {key} = params;
+        try {
+            await CacheService.addOrUpdateValue(key);
+
+            res.status(204).end();
+        } catch (e) {
+            next(e);
+        }
     },
 
     async deleteValue(req: Request, res: Response, next: NextFunction) {
-        next(new NotImplementedError())
+        const {params} = req;
+        const {key} = params;
+        try {
+            await CacheService.deleteValueByKey(key);
+
+            res.status(204).end();
+        } catch (e) {
+            next(e);
+        }
     },
 
     async flush(req: Request, res: Response, next: NextFunction) {
-        next(new NotImplementedError())
+        try {
+            await CacheService.flushCache();
+
+            res.status(200).end();
+        } catch (e) {
+            next(e);
+        }
     }
 }
